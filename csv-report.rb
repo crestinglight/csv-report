ddbd24ef4b00108026fac978a0d034e89a70ddbd
Evin Grost <evin.grost@gmail.com>
2017-01-26 16:41:58 -0600

    Some restructuring done.

--- csv-report.rb
+++ csv-report.rb
@@ -1,6 +1,23 @@
 require 'csv'
 require 'pry'
 
+args = ARGV
+csvFile="accounts.csv"
+
+accountCategories = getCategories(csvFile)
+totalSpent = 0
+startingBalance = getStartingBalance(accountName,csvFile)
+balanceRemaining = 0
+
+categoryList = []
+categoryBalanceList = []
+categoryCountList = []
+categoryAvgList = []
+
+
+categoryTransactionCount = 0
+categoryBalance = 0
+
 def getAccounts(inputCSV)
     accounts = []
     CSV.foreach(inputCSV, {headers: true, return_headers: false}) do |row|
@@ -50,41 +67,83 @@ def getStartingBalance(name, inputCSV)
     return transactions[0]["Inflow"].gsub(/[$]/,'').gsub(/[,]/,'').to_f
 end
 
-csvFile="accounts.csv"
+def convertToFloat(transaction)
+    transaction.gsub(/[$]/,'').gsub(/[,]/,'').to_f
+end
 
-accountsArray = getAccounts(csvFile)
+def processTransactions(categoryTransactions, categoryBalance, categoryTransactionCount, categoryCountList, categoryBalanceList)
+    for transaction in categoryTransactions
+        #Loops for each transaction for Priya and Sonia. Stripping the "$" and "," then converting to integer.
+        transactionOutflow = convertToFloat(transaction[4])
+        transactionInflow = convertToFloat(transaction[5])
+
+        transactionCountUpdate(categoryTransactionCount, categoryCountList)
+        balanceUpdate(transactionInflow, transactionOutflow, categoryBalance, categoryBalanceList)
+    end
+end
+
+def transactionCountUpdate(categoryTransactionCount, categoryCountList)
+    categoryTransactionCount = categoryTransactionCount + 1
+    categoryCountList.push(categoryTransactionCount)
+end
+
+def balanceUpdate(transactionInflow, transactionOutflow, categoryBalance, categoryBalanceList)
+    categoryBalance = categoryBalance  - transactionOutflow + transactionInflow
+    categoryBalanceList.push(categoryBalance.round(2))
+end
+
+
+
+if args.length > 0 
+    #if we are passed an account name in the command line
+    #only loop over the single account
+    accountsArray = [args[0]]
+else
+    #loop over all the accounts
+    accountsArray = getAccounts(csvFile)
+end
+
+    #console output stuff goes here
+    #remember to skip outputting a category if categoryCountList[i] == 0
+def consoleHeader(accountName, balanceRemaining)
+    print ("=" * 80) + "\n"
+    print "Account: " + accountName + "... Balance: $" + balanceRemaining.round(2).to_s + "\n"
+    print ("=" * 80) + "\n"
+    print "Category" + (" " * 22) + "|  " + "Total Spent" + (" " * 4) + "|  " + "Average Transaction" + "\n"
+    print ("-" * 29) + " | " + ("-" * 15) + " | " + ("-" * 30) + "\n"
+end
+
+def consoleBody(categoryList, categoryCountList, categoryBalanceList, categoryAvgList)
+    for i in 0..categoryList.length-1
+        if categoryCountList[i] > 0 then
+            print categoryList[i] + (" " * (30 - categoryList[i].length)) + "|  " + categoryBalanceList[i].round(2).to_s + (" " * (15 - categoryBalanceList[i].to_s.length)) + "|  " + categoryAvgList[i].round(2).to_s + "\n" 
+        end 
+    end
+    print "\n" + "\n"
+end
+
+
+def consoleOutput(accountName, balanceRemaining, categoryList, categoryCountList, categoryBalanceList, categoryAvgList)
+    consoleHeader(accountName, balanceRemaining)
+    consoleBody(categoryList, categoryCountList, categoryBalanceList, categoryAvgList)
+end
+
+
 for accountName in accountsArray 
     #For loop that will loop twice, once for each account name, in this case "Priya" and "Sonia".
     
-    accountCategories = getCategories(csvFile)
-    totalSpent = 0
-    startingBalance = getStartingBalance(accountName,csvFile)
-    balanceRemaining = 0
+    
 
-    categoryList = []
-    categoryBalanceList = []
-    categoryCountList = []
-    categoryAvgList = []
+    
 
     for category in accountCategories
         categoryTransactions = listTransactions(accountName, category, csvFile)
-        categoryTransactionCount = 0
-        categoryBalance = 0
         categoryList.push(category)
-   
-        for transaction in categoryTransactions
-            #Loops for each transaction for Priya and Sonia. Stripping the "$" and "," then converting to integer.
-            transactionCategory = transaction[3]
-            transactionOutflow = transaction[4].gsub(/[$]/,'').gsub(/[,]/,'').to_f
-            transactionInflow = transaction[5].gsub(/[$]/,'').gsub(/[,]/,'').to_f
-            categories = getCategories(csvFile)
-            #binding.pry
-            categoryTransactionCount = categoryTransactionCount + 1
-            categoryBalance = categoryBalance  - transactionOutflow + transactionInflow
-
-        end
-        categoryBalanceList.push(categoryBalance.round(2))
-        categoryCountList.push(categoryTransactionCount)
+   ###################################
+        
+        #Calling the for loop that processes each transaction for a category.
+        processTransactions(categoryTransactions, categoryBalance, categoryTransactionCount, categoryCountList, categoryBalanceList)
+        binding.pry
 
         if categoryTransactionCount > 0 
             categoryAvg = categoryBalance / categoryTransactionCount
@@ -100,24 +159,7 @@ for accountName in accountsArray
     for categoryBalance in categoryBalanceList
         balanceRemaining = balanceRemaining + categoryBalance
     end
-    
-    #console output stuff goes here
-    #remember to skip outputting a category if categoryCountList[i] == 0
-    print ("=" * 80) + "\n"
-    print "Account: " + accountName + "... Balance: $" + balanceRemaining.round(2).to_s + "\n"
-    print ("=" * 80) + "\n"
-    print "Category" + (" " * 22) + "|  " + "Total Spent" + (" " * 4) + "|  " + "Average Transaction" + "\n"
-    print ("-" * 29) + " | " + ("-" * 15) + " | " + ("-" * 30) + "\n"
-    for i in 0..categoryList.length-1
-        
-        #binding.pry
-        if categoryCountList[i] > 0 then
-            
-            print categoryList[i] + (" " * (30 - categoryList[i].length)) + "|  " + categoryBalanceList[i].round(2).to_s + (" " * (15 - categoryBalanceList[i].to_s.length)) + "|  " + categoryAvgList[i].round(2).to_s + "\n" 
-            #binding.pry
-        end 
+    consoleOutput(accountName, balanceRemaining, categoryList, categoryCountList, categoryBalanceList, categoryAvgList)
+end    
 
-    end
-    print "\n" + "\n"
 
-end
